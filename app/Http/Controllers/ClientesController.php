5307aea1841a97887b8936af930a5e12c6a0f88a
Milton Procel M <miltonprocel@hotmail.com>
2023-08-31 18:56:24 -0500

    Fin rutas

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -27,17 +27,44 @@ class ClientesController extends Controller
     {
         try {
             $nuevaPersona = new Clientes();
+            
             if($body->referencias){
-                $nuevaPersona ->referencias = $body->referencias;
+                if(is_string($body->referencias)){
+                    $nuevaPersona ->referencias = $body->referencias;
+                    return "Referencias actualizadas correctamente";
+                }else{
+                    return "Las referencias deben ser una cadena de texto";
+                }
+                
             }
             if($body->historial_de_compras){
-                $nuevaPersona ->historial_de_compras = $body->historial_de_compras;
+                if(is_string($body->historial_de_compras)){
+                    $nuevaPersona ->historial_de_compras = $body->historial_de_compras;
+                    return "Historial de compras actualizadas correctamente";
+                }else{
+                    return "El Historial de compras debe ser una cadena de texto";
+                }
             }
             if($body->Nivel_de_satisfacción){
-                $nuevaPersona ->Nivel_de_satisfacción = $body->Nivel_de_satisfacción;
+                if(is_string($body->Nivel_de_satisfacción)){
+                    $nuevaPersona ->Nivel_de_satisfacción = $body->Nivel_de_satisfacción;
+                    return "Nivel de satisfacción actualizadas correctamente";
+                }else{
+                    return "El Nivel de satisfacción debe ser una cadena de texto";
+                }
+                
             }
             if($body->Comentarios_observaciones){
-                $nuevaPersona ->Comentarios_observaciones = $body->Comentarios_observacionesx;
+                $nuevaPersona ->Comentarios_observaciones = $body->Comentarios_observaciones;
+            }
+            if($body->Comentarios_observaciones){
+                if(is_string($body->Comentarios_observaciones)){
+                    $nuevaPersona ->Comentarios_observaciones = $body->Comentarios_observaciones;
+                    return "Comentarios u observaciones actualizados correctamente";
+                }else{
+                    return "Comentarios u observaciones debe ser una cadena de texto";
+                }
+                
             }
         
             $nuevaPersona->save();
@@ -96,22 +123,49 @@ class ClientesController extends Controller
 
                 $edita = Clientes::find($id);
                 if($request->referencias){
-                    $edita ->referencias = $request->referencias;
+                    if(is_string($request->Nivel_de_satisfacción)){
+                        $edita ->referencias = $request->referencias;
+                        return "Referencias actualizadas correctamente";
+                    }else{
+                        return "Las referencias deben ser una cadena de texto";
+                    }
+                    
                 }
                 if($request->historial_de_compras){
-                    $edita ->historial_de_compras = $request->historial_de_compras;
+                    if(is_string($request->historial_de_compras)){
+                        $edita ->historial_de_compras = $request->historial_de_compras;
+                        return "Historial de compras actualizadas correctamente";
+                    }else{
+                        return "El Historial de compras debe ser una cadena de texto";
+                    }
                 }
                 if($request->Nivel_de_satisfacción){
-                    $edita ->Nivel_de_satisfacción = $request->Nivel_de_satisfacción;
+                    if(is_string($request->Nivel_de_satisfacción)){
+                        $edita ->Nivel_de_satisfacción = $request->Nivel_de_satisfacción;
+                        return "Nivel de satisfacción actualizadas correctamente";
+                    }else{
+                        return "El Nivel de satisfacción debe ser una cadena de texto";
+                    }
+                    
                 }
                 if($request->Comentarios_observaciones){
-                    $edita ->Comentarios_observaciones = $request->Comentarios_observacionesx;
+                    if(is_string($request->Comentarios_observaciones)){
+                        $edita ->Comentarios_observaciones = $request->Comentarios_observaciones;
+                        return "Comentarios u observaciones actualizados correctamente";
+                    }else{
+                        return "Comentarios u observaciones debe ser una cadena de texto";
+                    }
+                    
                 }
                 if($request->state){
-                    if($request->state != 0 || $request->state != 1){
-                        return "Recuerda que solo puede ser 0 o 1. Donde 0 es desactivado y 1 activado";
-                    }else{
-                        $edita ->state = $request->state;    
+                    if (strlen($request->referencias) == 1) {
+                        if($request->state != 0 || $request->state != 1){
+                            return "Recuerda que solo puede ser 0 o 1. Donde 0 es desactivado y 1 activado";
+                        }else{
+                            $edita ->state = $request->state;    
+                        }
+                    } else {
+                        return  'El estado debe tener 1 carácter';
                     }
                 }
             
