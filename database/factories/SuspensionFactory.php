a8f0f263ade8201108fa484578136bb49e3fe28d
Carlos <deleonc72@gmail.com>
2023-08-31 09:01:01 -0600

    migrations and seeders of marcas, modelos, cajas, suspensiones, combustibles were added

---
+++ database/factories/SuspensionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Suspension>
+ */
+class SuspensionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => fake()->word(), 
+        ];
+    }
+}
