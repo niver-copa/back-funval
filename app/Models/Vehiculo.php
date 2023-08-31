196fbe8740ba9c1ca9d89133be5d0858c5497da9
SirBeho <benjamin.tavarez.98@gmail.com>
2023-08-31 17:18:19 -0400

    modelo vehiculos

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehiculo extends Model
 {
@@ -31,5 +33,37 @@ class Vehiculo extends Model
         'sucursal_id'
     ];
 
+
+    public function modelo() :BelongsTo
+    {
+        return $this->belongsTo(modelo::class, 'modelo_id');
+    }
+
+    public function combustible() :BelongsTo
+    {
+        return $this->belongsTo(Combustible::class, 'combustible_id');
+    }
+
+
+    public function delantera_suspension() :BelongsTo
+    {
+        return $this->belongsTo(suspension::class, 'delantera_suspension_id');
+    }
+
+
+    public function trasera_suspension() :BelongsTo
+    {
+        return $this->belongsTo(suspension::class, 'trasera_suspension_id');
+    }
+
+    public function sucursal() :BelongsTo
+    {
+        return $this->belongsTo(sucursal::class, 'sucursal_id');
+    }
+
+
+
+
+    
 
 }
