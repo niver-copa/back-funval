3227d6890e73ab1e50583b4370f730d8bb5d92fc
Niver <vlader122@gmail.com>
2023-09-01 16:23:51 -0400

    [nx] fix vendedor routes and cliente controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ClientesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::controller(ClientesController::class)->group(function(){
-    Route::get('clientes', "show");
 });
