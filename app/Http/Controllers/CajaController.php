3e030b1c44b94adfea97835a0f1404c01996bbc5
Carlos <deleonc72@gmail.com>
2023-09-04 15:30:48 -0600

    Se agregaron metodos y rutas para obtener la lista de combustibles y tipos de caja

--- app/Http/Controllers/CajaController.php
+++ app/Http/Controllers/CajaController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Caja;
 use Illuminate\Http\Request;
 
 class CajaController extends Controller
 {
-    //
+    public function index()
+    {
+        $cajas = Caja::all();
+
+        return $cajas;
+    }
 }
